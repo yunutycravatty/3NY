ee246051fbd4decff010ed964150f746b2522e38
ploehnnico <ploehnnico@gmail.com>
2023-12-16 01:47:43 +0100

    configfile

--- src/config.py
+++ src/config.py
@@ -1,1 +1,5 @@
-INSTRUCTIONS=''
+INSTRUCTIONS='/recourses/gpt_preprompt.txt'
+with open('/resources/.OPENAI_API_KEY') as f:
+	key = ''.join([s for s in f.readlines()])
+OPENAI_API_KEY = key
+MODEL = "gpt-4-1106-preview"
