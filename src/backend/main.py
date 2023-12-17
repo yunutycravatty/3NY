b8367a1a7530864bfd5143813b7825c0b2d0348a
Yunus Saracoglu <saracoyu@gmail.com>
2023-12-17 10:56:00 +0100

    implemented image upload

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 
 from src.backend.router.gpt_request_route import gpt_request_route
+from src.backend.router.upload_image_route import upload_image_route
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    app.register_blueprint(upload_image_route)
     app.register_blueprint(gpt_request_route)
     return app
 
---
+++ src/backend/router/upload_image_route.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify, redirect
+
+from src.backend.services.gpt_request_service import gptRequestService
+
+upload_image_route = Blueprint('upload_image_route', __name__, url_prefix='/api/upload-image')
+
+@upload_image_route.route('/', methods=['POST'])
+def upload_image():
+    # get img path from request
+    data = request
+
+    # post to gpt_request_route message = <image uploaded>
+    message = { 'message': '<image uploaded>' }
+    answer, sendpdf = gptRequestService.process_message(message)
+
+    # return response
+    return jsonify({'answer': 'image uploaded'}), 200
+
+
